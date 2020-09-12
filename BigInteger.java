d07c654a9da4376fe0177488f65a10c3e576ebe6
annkit-singh <ankit871681@gmail.com>
2020-09-12 23:56:35 +0530

    Big Integer

---
+++ BigInteger.java
@@ -0,0 +1,29 @@
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+import java.math.BigInteger;
+import java.nio.charset.StandardCharsets;
+public class Demo {
+    public static void main(String [] args){
+      BigInteger b1=new BigInteger("11");
+      BigInteger b2=new BigInteger("4");
+      BigInteger mod=b1.modPow(new BigInteger("2"),new BigInteger("23"));
+
+      System.out.println(mod);
+
+
+    //   BigInteger mod = b1.modPow(new BigInteger("2"), new BigInteger("23"));  //b1^2%23
+    //   BigInteger mod = b1.mod(b2);  b1%b2
+    //   BigInteger div=b1.divide(b2);
+    // BigInteger multi = b1.multiply(b2);
+    //   BigInteger rem=b1.remainder(b2);  b1%b2
+    //   BigInteger gcd=b1.gcd(b2);   gcd(b1.b2)
+    //   b1=b1.subtract(b2);
+    //   b1=b1.pow(1000000);  
+
+
+    //   System.out.println(multi);
+
+    }
+    
+}
