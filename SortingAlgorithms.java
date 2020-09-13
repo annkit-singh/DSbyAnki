51c7b406a3b8718def890d748fbe405a730a635e
annkit-singh <ankit871681@gmail.com>
2020-09-13 16:48:39 +0530

    Sorting Modified

--- SortingAlgorithms.java
+++ SortingAlgorithms.java
@@ -65,6 +65,52 @@ class SortingAlgorithms{
 
     //#####################################################################################################################
 
+  public static void merge(int array[], int l, int m, int r) { // #stable
+    int n1 = m - l + 1;
+    int n2 = r - m;
+    int L[] = new int[n1];
+    int R[] = new int[n2];
+    for (int i = 0; i < n1; ++i)
+      L[i] = array[l + i];
+
+    for (int j = 0; j < n2; ++j)
+      R[j] = array[m + 1 + j];
+
+    int i = 0;
+    int j = 0;
+    int k = l;
+    while (i < n1 && j < n2) {
+      if (L[i] <= R[j]) {
+        array[k] = L[i];
+        i++;
+      } else {
+        array[k] = R[j];
+        j++;
+      }
+      k++;
+    }
+    while (i < n1) {
+      array[k] = L[i];
+      i++;
+      k++;
+    }
+    while (j < n2) {
+      array[k] = R[j];
+      j++;
+      k++;
+    }
+  }
+
+  public void mergeSort(int arr[], int l, int r) {
+    if (l < r) {
+      int mid = (l + r) / 2;
+      mergeSort(arr, l, mid);
+      mergeSort(arr, mid + 1, r);
+      merge(arr, l, mid, r);
+    }
+  }
+//###############################################################################
+
     public int partition(int array[],int low,int high) {
         int pivot = array[high];  //it this case we choose pivot last element or left right most element of an array
         int i=low-1;
