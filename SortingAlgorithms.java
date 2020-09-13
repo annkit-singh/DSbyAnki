60ea5995f41fecdcf67e8f8da9365eb0a9e3823a
annkit-singh <ankit871681@gmail.com>
2020-09-13 15:59:37 +0530

    Sorting Algorithms

--- SortingAlgorithms.java
+++ SortingAlgorithms.java
@@ -1,144 +1,124 @@
 import java.util.Arrays;
-public class SelectionSort {
 
+class SortingAlgorithms{
 
-
-        public void bubbleSort(int [] array){
-            for(int i=0;i<array.length-1;i++){
-                for(int j=0;j<array.length-1;j++){
-                    if(array[j]>array[j+1]){
-                        int temp=array[j+1];
-                        array[j+1]=array[j];
-                        array[j]=temp;
-                    }
+    // TimeComplexity - O(n^2)          #stable
+    public void bubbleSort(int[] array) {               
+        for (int i = 0; i < array.length - 1; i++) {
+            for (int j = 0; j < array.length - 1; j++) {
+                if (array[j] > array[j + 1]) {
+                    int temp = array[j + 1];
+                    array[j + 1] = array[j];
+                    array[j] = temp;
                 }
             }
         }
+    }
+    // ############################################################################################################### 
+    //  Time O(n^2)
+    void selectionSort(int arr[]) {  
+        int n = arr.length;
+        for (int i = 0; i < n - 1; i++) {
+            int min_idx = i;
+            for (int j = i + 1; j < n; j++) {
+                if (arr[j] < arr[min_idx]) {
+                    min_idx = j;
 
-    
-       void selectionSort(int arr[]){
-           int n=arr.length;
-           for(int i=0;i<n-1;i++){
-               int min_idx=i;
-               for(int j=i+1;j<n;j++){
-                   if(arr[j]<arr[min_idx]){
-                    min_idx=j;
+                }
 
-                   }
-
-               }
-               int temp=arr[min_idx];
-               arr[min_idx]=arr[i];
-               arr[i]=temp;
-           }
-       }
-       public static void merge(int array[],int l,int m,int e){
-           int n1=m-l+1;
-           int n2=e-m;
-           int L[]=new int[n1];
-           int R[]=new int[n2];
-           for(int i =0;i<n1;++i){
-               L[i]=array[l+i];
-           }
-           for(int j =0;j<n2;++j){
-            R[j]=array[m+1+j];
-        }
-        int i=0;int j=0;
-        int k=l;
-        while(i<n1 && j<n2){
-            if(L[i]<=R[j]){
-            array[k]=L[i];
-            i++;
             }
+            int temp = arr[min_idx];
+            arr[min_idx] = arr[i];
+            arr[i] = temp;
+        }
+    }
 
+    //##################################################################################################################
+//    Time Complexity  O( nlogn) 
+    public static void merge(int array[], int l, int m, int r) {   //#stable
+        int L[]=Arrays.copyOfRange(array, l, m); 
+        int R[]=Arrays.copyOfRange(array, m, r+1);   //here we use r+1 because third arguments does not r's index
+        int i = 0;                                      //e.g ar={1,2,3,4,5}  copyofRange(ar,0,2) so it returns {1,2} only 
+        int j = 0;
+        int k = l;
+        while (i < L.length && j < R.length) {
+            if (L[i] <= R[j]) {
+                array[k] = L[i];
+                i++;     }
             else{
-                array[k]=R[j];
-                j++;
-            }
+                array[k] = R[j];
+                j++;  }
             k++;
         }
-        while(i<n1){
-            array[k]=L[i];
-            i++;
-            k++;
-        }
-        while(j<n2){
-            array[k]=R[j];
-            i++;
-            k++;
-        }
-       }
+        while (i < L.length){ array[k] = L[i];  i++;   k++; }
+        while (j < R.length){ array[k] = R[j];  j++;   k++; }
+    }
 
-       public  void mergeSort(int arr[],int l,int r){
-           if(l<r){
-               int mid=(l+r)/2;
-               mergeSort(arr,l,mid);
-               mergeSort(arr,mid+1,r);
-              merge(arr, l, mid, r);
-           }
-       }
-     
-      
-        // Prints the array 
-        void printArray(int arr[]) 
-        { 
-            int n = arr.length; 
-            for (int i=0; i<n; ++i) 
-                System.out.print(arr[i]+" "); 
-            System.out.println();  
-        } 
+    public void mergeSort(int arr[], int l, int r) { // here l=0 and r=array.length-1
+        if (l < r) {
+            int mid = (l + r) / 2;
+            mergeSort(arr, l, mid);
+            mergeSort(arr, mid + 1, r);
+            merge(arr, l, mid, r);
+        }
+    }
 
-        public void bubbleSort(int array[]){
-            int last_idx=array.length-1;
-            for(int i=0;i<=last_idx;i++){
-                for(int j=0;j<=last_idx-1;j++){
-                    if(array[j]>array[j+1]){
-                    int temp=array[j+1];
-                    array[j+1]=array[j];
-                    array[j]=temp;
-                    }
-                }
+    //#####################################################################################################################
+
+    public int partition(int array[],int low,int high) {
+        int pivot = array[high];  //it this case we choose pivot last element or left right most element of an array
+        int i=low-1;
+        for (int j = low; j <high; j++) {
+            if (array[j] < pivot) {
+                i++;
+               int var=array[j];
+               array[j]=array[i];
+               array[i]=var;
             }
         }
+        i++;
 
+        int temp = pivot;
+        pivot = array[i];
+        array[i] = temp;
 
-        public int  partition(int array[]){
-           int  n=array.length;
-           int low=0;
-            int high=(n-1);
-            int i=(low-1);
-            int pivot=array[high];
-            for(int j =low;j<=(high-1);i++){
-                if(array[j]<pivot){
-                    i++;
-                    int temp=array[j];
-                    array[j]=array[i];
-                    array[i]=temp;
-                }
-            }
-                int temp=pivot;
-                pivot=array[i+1];
-                array[i+1]=temp;
+        return i; // it will return the position next to the exact position of the pivot
 
-            return i+1;
+    }
 
+    public void quickSort(int array[], int low, int high) {//here low=0 ,high=array.length-1 Bestcase -O(nlogn) worst O(n^2) #unstable
+        if (low < high) {
+            int p = partition(array,low,high);
+            quickSort(array, low, p - 1);   // it will sort the left part of the p  (NOTE : p is not included)
+            quickSort(array, p+1 , high);
         }
+    }
 
-        public void quickSort (int array[],int low,int high){
-            if(low<high){
-                int p=partition(array);
-                quickSort(array,low,p-1);
-                quickSort(array,p+1,high);
+    // ####################################################################################################################
+
+    public void insertionSort(int array[]){  // Best Case O(n) Worst Case O(n^2)  #stable
+        for(int i=1;i<array.length;i++){
+            int current=array[i];
+            int j=i-1;
+            while(j>=0 && array[j]>current){
+                array[j+1]=array[j];
+                j--;
             }
+            array[j+1]=current;
         }
-      
-        // Driver code to test above 
-        public static void main(String args[]) 
-        { 
-            SelectionSort ob = new SelectionSort(); 
-            int arr[] = {64,25,12,22,11}; 
-          ob.mergeSort(arr, 0, arr.length);
-           ob.printArray(arr);
-        } 
-    } 
-    /* This code is contributed by Rajat Mishra*/
+    }
+// ##############################################################################################################################
+    public static void main(String[] args) {
+    SortingAlgorithms t=new SortingAlgorithms();
+    int array[]={98,46,54,33,63,2,66,78,96};
+    t.insertionSort(array);
+    t.selectionSort(array);
+    t.bubbleSort(array);
+
+    t.mergeSort(array, 0, array.length-1);
+    t.quickSort(array, 0, array.length-1);
+    System.out.println(Arrays.toString(array));
+
+
+    }
+}
